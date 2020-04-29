337ed4f71cda84f8149b679a926d300df941ad91
sharankhala <48086436+sharankhala@users.noreply.github.com>
2020-04-29 13:50:05 +0530

    Add files via upload

---
+++ DiagnosticCenter/src/main/java/org/com/dao/UserDao.java
@@ -0,0 +1,9 @@
+package org.com.dao;
+import org.com.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+@Repository
+
+public interface UserDao extends JpaRepository<User, String>{
+
+}
